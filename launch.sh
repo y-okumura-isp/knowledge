9ca050c41ffa92e67f5ada7c128afd51d7a98d34
y-okumura <y-okumura@isp.co.jp>
2025-06-02 10:32:30 +0900

    Update dependencies

--- launch.sh
+++ launch.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -eu
 
-mkdir -p third_party
-git clone --branch v1-dev https://github.com/y-okumura-isp/markedj.git third_party/markedj
+if [ ! -d third_party ]; then
+   mkdir -p third_party
+   git clone --branch v1-dev https://github.com/y-okumura-isp/markedj.git third_party/markedj
+fi
 
-export DUID=$(UID)
+export DUID=${UID}
 export DGID=$(id -g)
 
 docker-compose run --rm maven sh -c "cd third_party/markedj; mvn install -DskipTests=true -Dmaven.javadoc.skip=true"
-docker-compose run --rm maven mvn install -DskipTests=true -Dmaven.javadoc.skip=true -e
+docker-compose run --rm maven mvn install -DskipTests=true -Dmaaven.test.skip=true -Dmaven.javadoc.skip=true -e
 # docker-compose run --rm maven mvn clean test site -e
 docker-compose run --rm maven mvn clean package -e
 
