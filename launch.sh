59936e15e85f77c88c20beae1ee0e655b1f6fc6a
y-okumura <y-okumura@isp.co.jp>
2026-06-20 14:29:28 +0900

    Bind mvn cache directory

--- launch.sh
+++ launch.sh
@@ -7,6 +7,8 @@ if [ "${1:-}" = "--build-only" ]; then
    shift
 fi
 
+mkdir -p .m2
+
 if [ ! -d third_party ]; then
    mkdir -p third_party
    git clone --branch v1-dev https://github.com/y-okumura-isp/markedj.git third_party/markedj
@@ -14,9 +16,10 @@ fi
 
 export DUID=${UID}
 export DGID=$(id -g)
+MAVEN_REPO_LOCAL=/usr/src/mymaven/.m2/repository
 
-docker-compose run --rm maven sh -c "cd third_party/markedj; mvn install -DskipTests=true -Dmaven.javadoc.skip=true"
-docker-compose run --rm maven mvn install -DskipTests=true -Dmaaven.test.skip=true -Dmaven.javadoc.skip=true -e
+docker-compose run --rm maven sh -c "cd third_party/markedj; mvn -Dmaven.repo.local=${MAVEN_REPO_LOCAL} install -DskipTests=true -Dmaven.javadoc.skip=true"
+docker-compose run --rm maven sh -c "mvn -Dmaven.repo.local=${MAVEN_REPO_LOCAL} install -DskipTests=true -Dmaven.test.skip=true -Dmaven.javadoc.skip=true -e"
 # docker-compose run --rm maven mvn clean test site -e
 # docker-compose run --rm maven mvn clean package -e
 
